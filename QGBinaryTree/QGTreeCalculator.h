d55d0571b69d37b4b3cbd311cc202bce8c53cdc9
yasoudream <634358229@qq.com>
2019-04-21 13:48:04 +0800

    更新遍历方式选择界面
    更新遍历方式选择界面

--- QGBinaryTree/QGTreeCalculator.h
+++ QGBinaryTree/QGTreeCalculator.h
@@ -27,10 +27,16 @@ public:
 	bool input(const char* equ);
 	bool output(float& Rgetter);
 
-	bool tr(void(*visit)(CalculateData))
-	{
-		return tree.InOrderTraverse_cycle(visit);
-	}
+	bool pretr(void(*visit)(CalculateData)) { return tree.PreOrderTraverse(visit); }
+	bool intr(void(*visit)(CalculateData)) { return tree.InOrderTraverse(visit); }
+	bool posttr(void(*visit)(CalculateData)) { return tree.PostOrderTraverse(visit); }
+	bool leveltr(void(*visit)(CalculateData)) { return tree.LevelOrderTraverse(visit); }
+	bool pretrc(void(*visit)(CalculateData)) { return tree.PreOrderTraverse_cycle(visit); }
+	bool intrc(void(*visit)(CalculateData)) { return tree.InOrderTraverse_cycle(visit); }
+	bool posttrc(void(*visit)(CalculateData)) { return tree.PostOrderTraverse_cycle(visit); }
+	bool leveltrc(void(*visit)(CalculateData)) { return tree.LevelOrderTraverse_cycle(visit); }
+
+
 };
 
 
--- QGBinaryTree/main.cpp
+++ QGBinaryTree/main.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <conio.h>
 #include "QGTreeCalculator.h"
 
 using namespace std;
@@ -9,13 +10,13 @@ void outputdata(CalculateData data)
 	switch (data.type)
 	{
 	case DataType::FLOAT:
-		cout << static_cast<float> (data.getvalue()) << endl; 
+		cout << static_cast<float> (data.getvalue()) << ' '; 
 		break;
 	case DataType::INT:
-		cout << static_cast<int> (data.getvalue()) << endl;
+		cout << static_cast<int> (data.getvalue()) << ' ';
 		break;
 	case DataType::OPERATOR:
-		cout << static_cast<char> (data.getvalue()) << endl;
+		cout << static_cast<char> (data.getvalue()) << ' ';
 		break;
 	}
 }
@@ -23,8 +24,49 @@ void outputdata(CalculateData data)
 
 int main()
 {
-	cout << "请输入表达式 负数请用括号\n输入错误指令即停止" << endl;
+	char c = 0;
+	bool (QGTreeCalculator::*func)(void(*visit)(CalculateData));
+	func = nullptr;
 	QGTreeCalculator ctr;
+	while (c > '8' || c < '1')
+	{
+		cout << "输入遍历模式 1~4 前序、中序、后序、层次遍历" << endl;
+		cout << "             5~8 上述遍历纯循环版" << endl;
+		c = _getch();
+	}
+	switch (c)
+	{
+	case '1':
+		func = &QGTreeCalculator::pretr;
+		break;
+	case '2':
+		func = &QGTreeCalculator::intr;
+		break;
+	case '3':
+		func = &QGTreeCalculator::posttr;
+		break;
+	case '4':
+		func = &QGTreeCalculator::leveltr;
+		break;
+	case '5':
+		func = &QGTreeCalculator::pretrc;
+		break;
+	case '6':
+		func = &QGTreeCalculator::intrc;
+		break;
+	case '7':
+		func = &QGTreeCalculator::posttrc;
+		break;
+	case '8':
+		func = &QGTreeCalculator::leveltrc;
+		break;
+	}
+
+
+	system("cls");
+
+	cout << "请输入表达式 负数请用括号\n输入错误指令即停止" << endl;
+	
 	float num;
 	char ch[MAX_LEN];
 	cin >> ch;
@@ -32,7 +74,9 @@ int main()
 	{
 		if (ctr.output(num))
 		{
-			ctr.tr(outputdata);
+			cout << "遍历结果" << endl;
+			(ctr.*func)(outputdata);
+			putchar('\n');
 			cout << "结果：" << num << endl;
 		}
 			
